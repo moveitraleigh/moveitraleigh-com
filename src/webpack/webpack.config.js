8b8bec196807cc0fca4e63ff84e9e75dcf8299ea
Brett A. Lewis <brett@pendo.io>
2018-11-01 20:43:07 -0400

    Fixes to netlify.toml

--- src/webpack/webpack.config.js
+++ src/webpack/webpack.config.js
@@ -3,6 +3,7 @@ const webpack = require('webpack')
 module.exports = {
   mode: "development",
   plugins: [
+    new webpack.DefinePlugin({ "BRANCH_ENV": process.env.BRANCH.toUpperCase() }),
     new webpack.DefinePlugin({ "global.GENTLY": false })
   ],
 }
