71e1abcc256a43c4a7553a0187d2acff3a428a78
Brett A. Lewis <brett@pendo.io>
2018-11-01 21:18:56 -0400

    Trying to set up form.

--- src/lambda/checkout.js
+++ src/lambda/checkout.js
@@ -2,6 +2,7 @@
 
 import SquareConnect from 'square-connect';
 import uuid from 'uuid';
+import querystring from 'querystring';
 
 const defaultClient = SquareConnect.ApiClient.instance;
 
@@ -10,41 +11,56 @@ oauth2.accessToken = process.env[`${BRANCH_ENV}_APP_ID`];
  
 const locationId = process.env[`${BRANCH_ENV}_LOC_ID`];
 
-const buyerInfo = {
-  buyer_email_address: 'thebuyer@example.com',
-  billing_address: {
-    address_line_1: '500 Electric Ave',
-    address_line_2: 'Suite 600',
-    administrative_district_level_1: 'NY',
-    locality: 'New York',
-    postal_code: '20003',
-    country: 'US'
-  }
-};
+exports.handler = async function(event, context) {
+  
+    // Only allow POST
+    if (event.httpMethod !== 'POST') {
+        return { statusCode: 405, body: 'Method Not Allowed' };
+    }
 
-const cardNonce = 'fake-card-nonce-ok';
-var idempotencyKey = uuid();
+    // When the method is POST, the name will no longer be in the event’s
+    // queryStringParameters – it’ll be in the event body encoded as a query string
+    const params = querystring.parse(event.body);
+    
+    return {
+        statusCode: 200,
+        body: JSON.stringify(params)
+    };
+  
+    const buyerInfo = {
+        buyer_email_address: 'thebuyer@example.com',
+        billing_address: {
+            address_line_1: '500 Electric Ave',
+            address_line_2: 'Suite 600',
+            administrative_district_level_1: 'NY',
+            locality: 'New York',
+            postal_code: '20003',
+            country: 'US'
+        }
+    };
 
-const paymentInfo = {
-  idempotency_key: idempotencyKey,
-  amount_money: {
-    amount: 1000,
-    currency: 'USD',
-  },
-  card_nonce: cardNonce
-};
+    const cardNonce = 'fake-card-nonce-ok';
+    var idempotencyKey = uuid();
 
-const referenceInfo = {
-  reference_id: 'Ref #12345',
-  note: 'Selected a $10 donation'
-};
+    const paymentInfo = {
+        idempotency_key: idempotencyKey,
+        amount_money: {
+            amount: 1000,
+            currency: 'USD',
+        },
+        card_nonce: cardNonce
+    };
 
-const transactionInfo = Object.assign({}, buyerInfo, paymentInfo, referenceInfo);
+    const referenceInfo = {
+        reference_id: 'Ref #12345',
+        note: 'Selected a $10 donation'
+    };
 
-const api = new SquareConnect.TransactionsApi();
+    const transactionInfo = Object.assign({}, buyerInfo, paymentInfo, referenceInfo);
 
-exports.handler = async function(event, context, callback) {
+    const api = new SquareConnect.TransactionsApi();
+
     return api.charge(locationId, transactionInfo)
-        .then((data) => callback(null, {statusCode: 200, body: JSON.stringify(data)}))
-        .catch((error) => callback({status: 500, body: JSON.stringify(Object.assign({}, error, transactionInfo, {locationId}))}));
+        .then((data) => ({statusCode: 200, body: JSON.stringify(data)}))
+        .catch((error) => ({statusCode: 500, body: JSON.stringify(error)}));
 };
